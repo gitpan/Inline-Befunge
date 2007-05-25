92bde135f50d047002d5e5456cc4841e439b7c9b
Jerome Quelin <jquelin@cpan.org>
2007-05-25 15:22:01 +0000

    Import of JQUELIN/Inline-Befunge-0.1.1 from CPAN.
    gitpan-cpan-distribution: Inline-Befunge
    gitpan-cpan-version:      0.1.1
    gitpan-cpan-path:         JQUELIN/Inline-Befunge-0.1.1.tar.gz
    gitpan-cpan-author:       JQUELIN
    gitpan-cpan-maturity:     released

--- lib/Inline/Befunge.pm
+++ lib/Inline/Befunge.pm
@@ -1,21 +1,9 @@
-#!perl
 #
 # This file is part of Inline::Befunge.
 # Copyright (c) 2001-2007 Jerome Quelin, all rights reserved.
 #
 # This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or (at
-# your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+# it under the same terms as Perl itself.
 #
 #
 
@@ -28,7 +16,7 @@ use Carp;
 use Language::Befunge;
 require Inline; # use Inline forbidden.
 our @ISA = qw! Inline !; # not "use base" (use will take precedence over require)
-our $VERSION   = '0.1.0';
+our $VERSION   = '0.1.1';
 
 
 sub register {
@@ -378,21 +366,10 @@ Jerome Quelin, C<< <jquelin at cpan.org> >>
 
 =head1 COPYRIGHT AND LICENCE
 
-Copyright (C) 2001-2007 Jerome Quelin
+Copyright (c) 2001-2007 Jerome Quelin, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+it under the same terms as Perl itself.
 
 
 =cut
